3fc72598fda9fe9a389101aa0527f8a918c2ceb2
KaikeRochaDev <kaikerochadev@gmail.com>
2023-03-10 20:05:22 -0300

    create mosquito function to move to random places

--- js/game.js
+++ js/game.js
@@ -9,3 +9,22 @@ function ajustaTamanhoPalcoJogo(){
 }
 
 ajustaTamanhoPalcoJogo()
+
+function posicaoRandomica(){
+    let posicaoX = Math.floor(Math.random() * largura) - 90
+    let posicaoY = Math.floor(Math.random() * altura) - 90
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+    let mosquito = document.createElement('img')
+    mosquito.src = 'imgs/mosca.png'
+    mosquito.className = "mosquito1"
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+
+    document.body.appendChild(mosquito)
+}
+
+posicaoRandomica()
