7e27513ce871e49b7010c5c4798e0aee3d69f6ba
KaikeRochaDev <kaikerochadev@gmail.com>
2023-03-11 15:36:30 -0300

    victory screen

--- js/game.js
+++ js/game.js
@@ -20,7 +20,7 @@ let cronometro = setInterval(function (){
     if(tempo < 0) {
         clearInterval(cronometro)
         clearInterval(criaMosquito)
-        alert("You win")
+        window.location.href = 'vitoria.html'
     } else {
         document.getElementById('cronometro').innerHTML = tempo
     }
